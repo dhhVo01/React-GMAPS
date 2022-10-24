be02de9d38b3f223d68f9b44fb7a3fe76aa7245b
Dangvhh <vohuynhhaidang24032001@gmail.com>
2022-10-24 12:27:19 +0700

    complete app

---
+++ client/src/components/Map.js
@@ -0,0 +1,99 @@
+import { useState, useMemo, useEffect } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+import { useGeolocated } from "react-geolocated";
+
+
+export default function Places() {
+  const [ libraries ] = useState(['places']);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GMAP_API_KEY,
+    libraries,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
+function Map() {
+    const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+        useGeolocated({
+            positionOptions: {
+                enableHighAccuracy: false,
+            },
+            userDecisionTimeout: 5000,
+        });
+    useEffect(() => {
+            if (coords){
+                const lat = coords.latitude;
+                const lng = coords.longitude;
+                setSelected({lat, lng});
+            };
+    }, [coords])
+    const [selected, setSelected] = useState(null);
+
+    return (
+        <>
+        <div className="places-container">
+            <PlacesAutocomplete setSelected={setSelected} />
+        </div>
+
+        <GoogleMap
+            zoom={10}
+            center={selected}
+            mapContainerClassName="map-container"
+        >
+            {selected && <Marker position={selected} />}
+        </GoogleMap>
+        </>
+    );
+}
+
+const PlacesAutocomplete = ({ setSelected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input"
+        placeholder="Search an address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyApp from "./components/Map";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <MyApp />
+);
